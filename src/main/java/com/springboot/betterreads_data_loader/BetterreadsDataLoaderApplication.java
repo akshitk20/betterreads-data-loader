0094ac3003a0266efaf0a6257646ea81c1ee0460
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-09-07 12:44:17 +0530

    initial commit

---
+++ src/main/java/com/springboot/betterreads_data_loader/BetterreadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.betterreads_data_loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterreadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	}
+
+}
